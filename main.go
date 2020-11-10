a44a390b9fa6cce18c1acc05b5e13c485cd07774
exactor96 <kyzya96@gmail.com>
2020-11-10 06:19:40 +0300

    test heroku

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	dev := os.Getenv("DEVELOPMENT")
+	var port int
+	if dev == "1" {
+		port = 8080
+	} else {
+		port = 80
+	}
+	fmt.Println(fmt.Sprintf("Working on port: %d", port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
 
 func main() {
-	fmt.Println("hello world")
+	handleRequests()
 }
